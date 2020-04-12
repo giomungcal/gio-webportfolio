33894f4c6626e8c77fd3f891a434c0e55ab614ae
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-04-12 02:47:39 -0600

    finalized dimensions and added links to github repos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,12 +18,14 @@ const Projects = () => {
             <p><strong>Developed with <span>Unity Engine</span></strong><br /><br />An adventure-based, interactive mobile quiz game that aims to assess the player's knowledge in the fundamentals of electronics related subjects.<br /><br />
               Includes 5 game types: <br />
               Multiple Choice, True or False, Guess the Symbol,  Identify the Resistance, and Word Translation (Filipino-English)</p>
+            <a className="projects__tile--repo" href="https://github.com/giomungcal/electro-knight" target="_blank"  rel="noopener noreferrer">open <span>github</span><br />repository</a>
           </div>
           <div className="projects__tile">
             <img className="projects__tile--title" src={cedulatitle} alt="cedula-title"/>
             <img  className="projects__tile--scs" src={cedulaimgs} alt="cedula"/><br />
             <p><strong>Developed with <span><br />HTML, CSS, JS</span></strong><br /><br />A system which provides a digital filing of forms for community tax certificate or cedula application, a computerized queueing stub, and a centralized database that will serve as a repository for all related transactions. 
             </p>
+            <a className="projects__tile--repo" href="https://github.com/giomungcal/cedula" target="_blank"  rel="noopener noreferrer">open <span>github</span><br />repository</a>
           </div>
           <div className="projects__tile">
             <img className="projects__tile--title" src={portfoliotitle} alt="portfolio-title"/>
@@ -32,6 +34,8 @@ const Projects = () => {
             <p><strong>Developed with <span><br />HTML, CSS, React JS <br /></span>Designed with <span className="adobexd">Adobe XD</span></strong><br /><br />
             This is my personal web portfolio developed and designed with the collective knowledge i have of web developmment so far.<br/><br/> I've applied my self-directed learnings, ranging from the different principles of UI designing to typography and color scheming, also, this webpage is designed responsive for different viewports, without the use of any framework.<br /><br />Used SASS, BEM. React JS implemented with Context API, Hooks, and Router.
             </p>
+            <br />
+            <a className="projects__tile--repo" href="https://github.com/giomungcal/gio-webportfolio" target="_blank"  rel="noopener noreferrer">open <span>github</span><br />repository</a>
           </div>
         </div>
     </div>
