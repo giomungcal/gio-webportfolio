633b601866039eff518dc5cf908d73bc6e4a5ebf
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-04-12 03:09:55 -0600

    Hovering img now clickable

--- src/components/HoveringImg.js
+++ src/components/HoveringImg.js
@@ -5,7 +5,7 @@ import hover from '../imgs/hover.png'
 const HoveringImg = () => {
     return (
         <React.Fragment>
-            <img className="hoverimg show-desktop hide-mobile" src={hover} alt="hover"/>
+            <a href="#skills"><img className="hoverimg show-desktop hide-mobile" src={hover} alt="hover"/></a>
         </React.Fragment>
     )
 }
