a1a2bf80b8e08b456f75fe9656a0bd008075e089
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-04-13 23:11:08 -0600

    Removed hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,19 @@ import hamburger from '../imgs/hamburger.png'
 
 const Navbar = () => {
     return (
-    <div className="header">
-        <h1 className="header__title">gio-portfolio</h1>
-        <nav className="header__nav">
-        <a href="#"><img className="header__hamburger hide-desktop" src={hamburger} alt="hamburger"/></a>
-        <ul className="header__list show-desktop hide-mobile">
-            <li className="hide-desktop"><a href="#home">X</a></li>
-            <li className="header__link header__link--active"><a href="#">home</a></li>
-            <li className="header__link"><a href="#skills">skills</a></li>
-            <li className="header__link"><a href="#projects">projects</a></li>
-            <li className="header__link"><a href="#about">about</a></li>
-        </ul>
-        </nav>
-    </div>
+        <div className="header">
+            <h1 className="header__title">gio-portfolio</h1>
+            <nav className="header__nav">
+            <a href="#"><img className="header__hamburger hide-desktop hide-mobile" src={hamburger} alt="hamburger"/></a>
+            <ul className="header__list show-desktop hide-mobile">
+                <li className="hide-desktop"><a href="#home">X</a></li>
+                <li className="header__link header__link--active"><a href="#">home</a></li>
+                <li className="header__link"><a href="#skills">skills</a></li>
+                <li className="header__link"><a href="#projects">projects</a></li>
+                <li className="header__link"><a href="#about">about</a></li>
+            </ul>
+            </nav>
+        </div>
     )
 }
 
