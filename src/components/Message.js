67c25b7523525eaefc2f2b64df478caff87a5598
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-04-23 02:18:48 -0600

    Fixed errors

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+const Emoji = props => (
+    <span
+        className="emoji"
+        role="img"
+        aria-label={props.label ? props.label : ""}
+        aria-hidden={props.label ? "false" : "true"}
+    >
+        {props.symbol}
+    </span>
+);
+
 const Message = () => {
     return (
         <div className="message">
             <h1>let's<br/>talk.</h1>
-            <h3><span role="img">📠</span> +639953650731<br/><span role="img">📪</span> gcmungcal@gmail.com</h3>
+            <h3><Emoji symbol="📠" label="telephone"/> +639953650731<br/><Emoji symbol="📪" label="mail"/> gcmungcal@gmail.com</h3>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,15 @@
 import React from 'react'
-
-import hamburger from '../imgs/hamburger.png'
-
+// import hamburger from '../imgs/hamburger.png'
 
 const Navbar = () => {
     return (
-        <div className="header">
+        <div className="header" id="nav">
             <h1 className="header__title">gio-portfolio</h1>
             <nav className="header__nav">
-            <a href="#"><img className="header__hamburger hide-desktop hide-mobile" src={hamburger} alt="hamburger"/></a>
+            {/* <a href="#"><img className="header__hamburger hide-desktop hide-mobile" src={hamburger} alt="hamburger"/></a> */}
             <ul className="header__list show-desktop hide-mobile">
                 <li className="hide-desktop"><a href="#home">X</a></li>
-                <li className="header__link header__link--active"><a href="#">home</a></li>
+                <li className="header__link header__link--active"><a href="#nav">home</a></li>
                 <li className="header__link"><a href="#skills">skills</a></li>
                 <li className="header__link"><a href="#projects">projects</a></li>
                 <li className="header__link"><a href="#about">about</a></li>
